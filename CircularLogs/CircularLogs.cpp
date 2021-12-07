659a806af48b3651b02d59e3bb5490abb42babeb
Nauman3S <naumanshakir3s@gmail.com>
2021-12-07 14:26:55 +0500

    updated code

--- CircularLogs/CircularLogs.cpp
+++ CircularLogs/CircularLogs.cpp
@@ -2,7 +2,7 @@
 
 int logCharPointer = 0;
 
-void writeToFile(char *data)
+void writeToFile(char *data) // a function to handle logs to file writing operations
 {
     FILE *filePtr;
     char filename[30];
@@ -24,9 +24,6 @@ int main()
 
     char LogEntriesStr[LOG_ENTRY_STR_SIZE];
 
-    int EntrylogTime = 1000 * 1; //1 ms
-    clock_t start_time = clock();
-
     initscr();
     noecho();
     // nodelay(stdscr, TRUE);
@@ -35,48 +32,40 @@ int main()
     int ch;
     for (;;)
     {
-        // if (clock() >= start_time + EntrylogTime) //try to get char after every 1 ms(timeout logic)
-        // {
-            ch = getch();
-            char logEntiresBuffer[LOG_ENTRIES_BUFFER_SIZE];
-            if (ch == 'S') //log file save mechanism
-            {
-                if (logCharPointer < 100)
-                { //if log entries are less than 100 chars, save from index 0 uptil the last entered char
-                    memcpy(logEntiresBuffer, &LogEntriesStr[0], logCharPointer);
-                    writeToFile(logEntiresBuffer);
-                }
-                else
-                { //save last 100 chars to file
 
-                    memcpy(logEntiresBuffer, &LogEntriesStr[logCharPointer - 100], 100);
-                    writeToFile(logEntiresBuffer);
-                }
-
-                //break;
+        ch = getch();
+        char logEntiresBuffer[LOG_ENTRIES_BUFFER_SIZE];
+        if (ch == 'S') //log file save mechanism
+        {
+            if (logCharPointer < 100)
+            { //if log entries are less than 100 chars, save from index 0 uptil the last entered char
+                memcpy(logEntiresBuffer, &LogEntriesStr[0], logCharPointer);
+                writeToFile(logEntiresBuffer); //save 100 char length buffer to the file
             }
-            else if (ch != ERR)
+            else
             {
-                printw("%c", ch); //show on screen whatever key is being pressed.
 
-                if (logCharPointer > LOG_ENTRY_STR_SIZE)
-                {
-                    logCharPointer = 0; //start overwriting
-                }
-                else
-                {
-
-                    LogEntriesStr[logCharPointer] = ch;
-                    logCharPointer++;
-                }
+                memcpy(logEntiresBuffer, &LogEntriesStr[logCharPointer - 100], 100); //copy the last 100 chars to a buffer
+                writeToFile(logEntiresBuffer);                                       //save 100 char length buffer to the file
             }
-            refresh();
-
-            // start_time = clock(); //reset timer
-
-          //  refresh();
         }
-    // }
+        else if (ch != ERR)//checks if something is pressed before the timeout (1ms)
+        {
+            printw("%c", ch); //show on screen whatever key is being pressed.
+
+            if (logCharPointer > LOG_ENTRY_STR_SIZE)
+            {
+                logCharPointer = 0; //start overwriting//reset the pointer
+            }
+            else
+            {
+
+                LogEntriesStr[logCharPointer] = ch;
+                logCharPointer++;
+            }
+        }
+        refresh();
+    }
     refresh();
     endwin();
 
--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-README
+<p align="center">
+  <a href="" rel="noopener">
+ <img width=200px height=200px src="temp/circularLogs.png" alt="Project logo"></a>
+</p>
+
+<h3 align="center">Circular Logs</h3>
+
+<div align="center">
+
+[![Status](https://img.shields.io/badge/status-active-success.svg)]()
+
+
+</div>
+
+---
+
+
+<p align="center"> Circular Logs
+    <br> 
+</p>
+
+## 📝 Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Prerequisites](#deployment)
+- [Installation and Config](#Installation_and_Config)
+- [Test/Usage](#test)
+- [Built Using](#built_using)
+- [Authors](#authors)
+
+## 🧐 About <a name = "about"></a>
+
+This repo contains C program Circular Logs task.
+
+## 🏁 Getting Started <a name = "getting_started"></a>
+
+These instructions will get you a copy of the project up and running on your local machine for testing.
+
+### Prerequisites <a name = "Prerequisites"></a>
+
+Things you need to install the program.
+
+```
+- Debain Based Distro(Ubuntu/Raspberry Pi OS etc.)
+```
+
+## Installation and Configuration <a name = "Installation_and_Config"></a>
+
+A step by step series that covers how to get the program running.
+
+### Installation
+
+1.  Open the terminal in the root directory of this repo and execute the following command
+   
+    ```make install```
+2. Now you can compile the program using
+
+    ```make all```
+
+## ⛏️ Testing/Usage <a name = "test"></a>
+
+The program can be tested on any Debian based Linux distribution with gcc and make.
+
+To run the program simply execute the following command in the root directory of this repo
+
+``make run``
+
+### Usage
+
+You can enter any keys on the keyboard and they will get logged to an char array LogEntriesStr which has a default size of 500(can be changed in CircularLogs.h file). On pressing 'S' (capital S) key on keybaord a new log file with latest 100 log entries will be saved in the logs directory of this repository.
+
+Once the LogEntriesStr fills out completely, the program starts overwriting the array from index 0 so that buffer overflow doesn't occur and the program should mimic limited flash memory scenario.
+
+## Libraries Used
+
+1.  ncurses(for getting the individual key presses from stdin){getc and other standard I/O fucntions waits until \n is pressed while getch of ncurses can read any key press event}
+2.  stdio(for File realted operations)
+3.  stdlib(for sizof and other related functions)
+4.  cstring(for memcpy)
+5.  time(for getting system clock time in ms)
+
+
+## ⛏️ Built Using <a name = "built_using"></a>
+
+- [C](#) - C Programming Language
+- [gcc](#) - Standard C compiler
+- [VSCode](#) - IDE
+
+## ✍️ Authors <a name = "authors"></a>
+
+- [@Nauman3S](https://github.com/Nauman3S) - Development
